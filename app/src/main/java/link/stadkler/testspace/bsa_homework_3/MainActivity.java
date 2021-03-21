c7b69f39a388bb2a5e5f5c1fbe57fdf79fe2a532
Niklas Jörg STADLER <wi19b065@technikum-wien.at>
2021-03-21 13:27:40 +0100

    Initial commit

---
+++ app/src/main/java/link/stadkler/testspace/bsa_homework_3/MainActivity.java
@@ -0,0 +1,14 @@
+package link.stadkler.testspace.bsa_homework_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
